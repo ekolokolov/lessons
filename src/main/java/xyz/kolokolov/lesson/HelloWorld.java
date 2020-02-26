efe74d65444d42df40955a35837997977ebdf60d
e.kolokolov <e.kolokolov@redmadrobot.com>
2020-02-26 18:20:02 +0300

    add HelloWorld

---
+++ src/main/java/xyz/kolokolov/lesson/HelloWorld.java
@@ -0,0 +1,9 @@
+package xyz.kolokolov.lesson;
+
+public class HelloWorld {
+
+    public static String getText() {
+        String result = "Hello " + " world";
+        return result;
+    }
+}
---
+++ src/main/java/xyz/kolokolov/lesson/TestHello.java
@@ -0,0 +1,9 @@
+package xyz.kolokolov.lesson;
+
+public class TestHello {
+
+    public static void main(String[] args) {
+        String text = HelloWorld.getText();
+        System.out.println(text);
+    }
+}
